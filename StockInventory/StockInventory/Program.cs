d156a993e5da8df374a9da78c3070595a38536b6
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-29 21:24:08 +0530

    [Sangmeshwar] Display:Displaying List Of All Stocks Customer Having

---
+++ StockInventory/StockInventory/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StockInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Welcome to Stock Inventory Management---");
+            string stockFilePath = @"C:\Users\hp\source\repos\StockInventory\StockInventory\Stock.json";
+            string customerFilePath = @"C:\Users\hp\source\repos\StockInventory\StockInventory\Customer.json";
+            StockAccountManagement management = new StockAccountManagement();
+            Console.WriteLine("\nList Of All Company Stock :");
+            management.ReadStockJsonFile(stockFilePath);
+            Console.WriteLine("\nList Of All Company Stock Customer Have :");
+            management.ReadCustomerJsonFile(customerFilePath);
+
+        }
+    }
+}
---
+++ StockInventory/StockInventory/StockAccountManagement.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace StockInventory
+{
+    class StockAccountManagement
+    {
+        double amount = 100;
+        List<StockDetails> stock = new List<StockDetails>();
+        List<StockDetails> customer = new List<StockDetails>();
+        public void ReadStockJsonFile(string filePath)
+        {
+            var json = File.ReadAllText(filePath);
+            this.stock = JsonConvert.DeserializeObject<List<StockDetails>>(json);
+            foreach(var content in stock)
+            {
+                //Console.WriteLine(content.StockName+" "+content.StockPrice+" "+content.NoOfShares);
+                Console.WriteLine("{0}" + "\t" + "{1}" + "\t" + "{2}", content.StockName, content.StockPrice, content.NoOfShares);
+            }
+        }
+        public void ReadCustomerJsonFile(string filePath)
+        {
+            var json = File.ReadAllText(filePath);
+            this.customer = JsonConvert.DeserializeObject<List<StockDetails>>(json);
+            foreach (var content in customer)
+            {
+                //Console.WriteLine(content.StockName + " " + content.StockPrice + " " + content.NoOfShares);
+                Console.WriteLine("{0}" + "\t" + "{1}" + "\t" + "{2}" + "\t\t" + "{3}", content.StockName, content.StockPrice, content.NoOfShares, content.NoOfShares * content.StockPrice);
+            }
+        }
+    }
+}
